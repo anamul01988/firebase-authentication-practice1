7c919d45ce824afcb586a91bfab2a3e4556841cd
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-08 23:19:11 +0600

    authentication added by github

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './firebase.init';
 import { useState } from 'react';
 
 const auth = getAuth(app);
 function App() {
   const [user, setUser] = useState();
+  const googleProvider = new GoogleAuthProvider();
+  const facebookProvider = new FacebookAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () =>{
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
+
+    signInWithPopup(auth, googleProvider)
   .then((result) => {
      const user = result.user;
      console.log(user)
@@ -20,9 +23,37 @@ function App() {
   });
 
   }
+  const handleFacebookSignIn = ()=>{
+    signInWithPopup(auth, facebookProvider)
+    // console.log(auth, facebookProvider)
+    .then(result=> {
+      const user = result.user;
+      
+      console.log(user)
+    })
+    .catch(error =>{
+      console.error(error);
+    })
+  }
+
+  const handleGithubSignIn = ()=>{
+      console.log('git')
+      signInWithPopup(auth, githubProvider)
+      // console.log(auth, facebookProvider)
+      .then(result=> {
+        const user = result.user;
+        
+        console.log(user)
+      })
+      .catch(error =>{
+        console.error(error);
+      })
+  }
   return (
     <div className="App">
         <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
+        <button onClick={handleGithubSignIn}>Github Sign In</button>
         <h3>Name: {user?.displayName}</h3>
         <img src={user?.photoURL} alt="tyuh" />
         <h4>Gmail: {user?.email}</h4>
