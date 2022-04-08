4f5c04f1634e8d639ccebbcdd5be60d24e61a36e
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-08 23:55:19 +0600

    sign out added from firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
-import './App.css';
-import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import app from './firebase.init';
-import { useState } from 'react';
+import "./App.css";
+import {
+  FacebookAuthProvider,
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "./firebase.init";
+import { useState } from "react";
 
 const auth = getAuth(app);
 function App() {
@@ -11,52 +17,68 @@ function App() {
   const facebookProvider = new FacebookAuthProvider();
   const githubProvider = new GithubAuthProvider();
 
-  const handleGoogleSignIn = () =>{
-
+  const handleGoogleSignIn = () => {
     signInWithPopup(auth, googleProvider)
-  .then((result) => {
-     const user = result.user;
-     console.log(user)
-     setUser(user);  //aita state a pathacci     
-  }).catch((error) => {
-     console.log(error);
-  });
-
-  }
-  const handleFacebookSignIn = ()=>{
-    signInWithPopup(auth, facebookProvider)
-    // console.log(auth, facebookProvider)
-    .then(result=> {
-      const user = result.user;
-      
-      console.log(user)
-    })
-    .catch(error =>{
-      console.error(error);
-    })
-  }
-
-  const handleGithubSignIn = ()=>{
-      console.log('git')
-      signInWithPopup(auth, githubProvider)
-      // console.log(auth, facebookProvider)
-      .then(result=> {
+      .then((result) => {
         const user = result.user;
-        
-        console.log(user)
+        console.log(user);
+        setUser(user); //aita state a pathacci
       })
-      .catch(error =>{
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider)
+      // console.log(auth, facebookProvider)
+      .then((result) => {
+        const user = result.user;
+
+        console.log(user);
+      })
+      .catch((error) => {
         console.error(error);
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    console.log("git");
+    signInWithPopup(auth, githubProvider)
+      // console.log(auth, facebookProvider)
+      .then((result) => {
+        const user = result.user;
+
+        console.log(user);
       })
-  }
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
+  };
   return (
     <div className="App">
-        <button onClick={handleGoogleSignIn}>Google Sign In</button>
-        <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
-        <button onClick={handleGithubSignIn}>Github Sign In</button>
-        <h3>Name: {user?.displayName}</h3>
-        <img src={user?.photoURL} alt="tyuh" />
-        <h4>Gmail: {user?.email}</h4>
+      {user?.displayName ? (
+        <button onClick={handleSignOut}> Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+      )}
+
+      <h3>Name: {user?.displayName}</h3>
+      <img src={user?.photoURL} alt="tyuh" />
+      <h4>Gmail: {user?.email}</h4>
     </div>
   );
 }
